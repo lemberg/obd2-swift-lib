7e1e6c5f870d01bd2e685c5c1f5b2c5050c87aa8
Sergiy Loza <kabal2015@gmail.com>
2017-05-29 12:24:36 +0300

    Small code formating

--- OBD2-Swift/OBD2-Swift/Model/Package.swift
+++ OBD2-Swift/OBD2-Swift/Model/Package.swift
@@ -9,70 +9,70 @@
 import Foundation
 
 struct Package {
-  var buffer : [UInt8]
-  var length : Int
-  
-  init(buffer : [UInt8], length : Int) {
-    self.buffer = buffer
-    self.length = length
-  }
-  
-  /*
-   const char* test = "41 00 90 18 80 00 \r41 00 BF 9F F9 91 ";
-   return [NSString stringWithCString:test encoding:NSASCIIStringEncoding];
-   */
-
-  var asciistr : [Int8]  {
-    return ascii()
-  }
-  
-  var strigDescriptor : String {
-    return String.init(cString: asciistr, encoding: String.Encoding.ascii) ?? ""
-  }
-  
-  private func ascii() -> [Int8] {
-    return buffer.map({Int8.init(bitPattern: $0)})
-  }
-  
-
-  var isOK :  Bool{
-    return strigDescriptor.contains("OK")
-  }
-  
-  var isError : Bool	{
-    return strigDescriptor.contains("?")
-  }
-  
-  var isStopped : Bool	{
-    return strigDescriptor.contains("STOPPED")
-  }
-  
-  var isNoData : Bool	{
-    return strigDescriptor.contains("NO DATA")
-  }
-  
-  var isSearching : Bool	{
-    return strigDescriptor.contains("SEARCHING...")
-  }
-  
-  func isAuto(_ str : String) -> Bool {
-    return strigDescriptor.hasPrefix("AUTO")
-  }
-  
-  var isData : Bool	{
-    let unwrapStr = strigDescriptor.characters.first ?? Character.init(" ")
-    let str = String(describing: unwrapStr)
-    let isDigit = Int(str) != nil
-    return isDigit
-  }
-  
-  var isAT : Bool	{
-    guard let char = asciistr.first else {return false}
-    guard let int32 = Int32.init(exactly: char) else {return false}
-    return isalpha(int32) == 0
-  }
-  
-  func isComplete() -> Bool {
-    return buffer.last == kResponseFinishedCode
-  }
+    
+    var buffer: [UInt8]
+    var length: Int
+    
+    init(buffer : [UInt8], length : Int) {
+        self.buffer = buffer
+        self.length = length
+    }
+    
+    /*
+     const char* test = "41 00 90 18 80 00 \r41 00 BF 9F F9 91 ";
+     return [NSString stringWithCString:test encoding:NSASCIIStringEncoding];
+     */
+    var asciistr: [Int8]  {
+        return ascii()
+    }
+    
+    var strigDescriptor: String {
+        return String(cString: asciistr, encoding: String.Encoding.ascii) ?? ""
+    }
+    
+    private func ascii() -> [Int8] {
+        return buffer.map({Int8.init(bitPattern: $0)})
+    }
+    
+    
+    var isOK: Bool{
+        return strigDescriptor.contains("OK")
+    }
+    
+    var isError: Bool	{
+        return strigDescriptor.contains("?")
+    }
+    
+    var isStopped: Bool	{
+        return strigDescriptor.contains("STOPPED")
+    }
+    
+    var isNoData: Bool	{
+        return strigDescriptor.contains("NO DATA")
+    }
+    
+    var isSearching: Bool	{
+        return strigDescriptor.contains("SEARCHING...")
+    }
+    
+    func isAuto(_ str : String) -> Bool {
+        return strigDescriptor.hasPrefix("AUTO")
+    }
+    
+    var isData: Bool	{
+        let unwrapStr = strigDescriptor.characters.first ?? Character(" ")
+        let str = String(describing: unwrapStr)
+        let isDigit = Int(str) != nil
+        return isDigit
+    }
+    
+    var isAT: Bool	{
+        guard let char = asciistr.first else { return false }
+        guard let int32 = Int32(exactly: char) else { return false }
+        return isalpha(int32) == 0
+    }
+    
+    func isComplete() -> Bool {
+        return buffer.last == kResponseFinishedCode
+    }
 }

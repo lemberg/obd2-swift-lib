1da6fef8a086ebe2bd32e43af1ef6a8550bcb9d4
OverSwift <kabal2015@gmail.com>
2017-11-01 12:24:40 +0200

    Fix string print warning

--- Example/OBD2-Swift/ViewController.swift
+++ Example/OBD2-Swift/ViewController.swift
@@ -135,7 +135,7 @@ class ViewController: UIViewController {
         }
         
         obd.request(command: Command.Custom.string("0902")) { (descr) in
-            print("Response \(descr?.getResponse())")
+            print("Response \(String(describing: descr?.getResponse()))")
         }
     }
     
